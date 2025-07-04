5e2dd5065c20bfbc0b3c54f2581e6ef7dd21a248
Mounika B <mounikab@kyndryl.com>
2025-07-04 17:45:24 +0530

    adding s3 and backend file

---
+++ backend.tf
@@ -0,0 +1,16 @@
+/*terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}*/
+
+terraform {
+  backend "s3" {
+    bucket        = "mounika-terraform-bucket101"
+    key           = "terraform.tfstate"
+    region        = "us-east-1"
+    encrypt       = true
+    use_lockfile  = true
+  }
+}
+
