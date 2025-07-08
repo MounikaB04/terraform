766bf30e1ab5845cd104be6930b43014d639c804
Mounika B <mounikab@kyndryl.com>
2025-07-08 11:50:34 +0530

    vars and output file

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "ec2" {
-  ami           = "ami-0c5d10c64897b15df"
+  ami           = var.ami    #"ami-0c5d10c64897b15df"
   instance_type = "t2.micro"
 
   tags = {
     Name = "ec2"
-    
+    created_by = var.creator
   }
 }
